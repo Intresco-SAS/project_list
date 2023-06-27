4dd5397dd1b238ba65ae012cd23a9a1224e9e3b4
Nelson V <nelson.avelez@hotmail.com>
2023-06-26 19:33:16 -0500

    14.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,21 +3,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Projects List View',
-    'version': '12.0.1.0.0',
-    'category': 'Project',
-    'website': 'https://github.com/OCA/project',
-    'author':
-        'CorporateHub, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Projects list view',
-    'depends': [
-        'project',
-    ],
-    'data': [
-        'views/project_project.xml',
-    ],
+    "name": "Projects List View",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "author": "CorporateHub, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Projects list view",
+    "depends": ["project"],
+    "data": ["views/project_project.xml"],
 }
